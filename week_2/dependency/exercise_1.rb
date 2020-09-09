32bb58889a4d1a1fff3dd3ce0cda73f57aec6a6a
Hilda Amponsah <hilda.amp@hotmail.com>
2020-09-09 11:02:17 +0100

    dependency injection-added a second method

--- week_2/dependency/exercise_1.rb
+++ week_2/dependency/exercise_1.rb
@@ -2,17 +2,30 @@ class Spanish
   def greet
     'Hola amiga, como estas?'
   end
+ 
+  def feel
+    'esta bien'
+  end
 end
 
 class French
   def greet
     'Bonjour, commo ça va?'
   end
+
+  def feel
+    'très bien'
+  end
+  
 end
 
 class Irish
   def greet
     'Dia dhuit, conas atá tú?'
+  end
+
+  def feel
+    'tá me go mhaith'
   end
 end
 
@@ -20,8 +33,11 @@ end
 class Languages
       # injecting dependenncy(injecting the arguement)
   def greeter(language)
-
     "In this language, we say hello, how are you like this:#{language.greet}"
+  end
+
+  def feels(feeling)
+    "Testing if this works for feelinngs: #{feeling.feel}"
   end
 end
 
@@ -31,4 +47,4 @@ spanish = Spanish.new
 french = French.new
 
 p greeting.greeter(french)
-p greeting.greeter(irish)
+p greeting.feels(french)
